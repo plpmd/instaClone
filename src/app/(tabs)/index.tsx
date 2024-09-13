0ff0788a43f976eee3f93f61d25f936cd74713f3
Pedro Luan <pedro.dias@olxbr.com>
2024-09-13 13:24:54 -0300

    feat: web feedScreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,9 +4,10 @@ import PostListItem from '@/src/components/PostListItem';
 
 export default function FeedScreen() {
   return (
-    <FlatList 
+    <FlatList
+      className='items-center'
       data={posts}
-      contentContainerStyle={{ gap: 10}}
+      contentContainerStyle={{ gap: 10, maxWidth: 512, width: '100%'}}
       renderItem={({ item }) => <PostListItem post={item} />}
       showsVerticalScrollIndicator={false}
     />
