931d5b3e9e0ffe6b84b00d78005c5c56ed9070d9
Pedro Luan <pedro.dias@olxbr.com>
2024-09-13 17:41:11 -0300

    fix: text bug

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,7 +5,7 @@ import AuthProvider from '../providers/AuthProvider';
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }} />;
+      <Stack screenOptions={{ headerShown: false }} />
     </AuthProvider>
   )
 }
