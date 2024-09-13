4a9d402da79f255a82026426b1d50894c373eca2
Pedro Luan <pedro.dias@olxbr.com>
2024-09-13 15:08:43 -0300

    feat: select image

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,15 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Image, Pressable, Text, TextInput, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function CreatePost() {
-  const [caption, setCaption] = useState('')
+  const [caption, setCaption] = useState<string | null>(null)
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: false,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  useEffect(() => {
+    if (!image) {
+      pickImage()
+    }
+  }, [image])
 
   return <View className='p-3 items-center flex-1'>
-    <Image source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg" }}
-      className='w-52 aspect-[3/4] rounded-lg'
-    />
+    {image ? 
+      <Image source={{ uri: image }}
+        className='w-52 aspect-[3/4] rounded-lg bg-slate-300'
+      />
+      :
+      <View className='w-52 aspect-[3/4] rounded-lg bg-slate-300' />
+    }
 
-    <Text onPress={() => { }} className='text-blue-500 font-semibold m-5'>
+    <Text onPress={pickImage} className='text-blue-500 font-semibold m-5'>
       Change
     </Text>
 
